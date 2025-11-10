1430014c411b47e79b1987c92c4783efc2504aa0
APigott1 <pigster99@gmail.com>
2025-11-10 11:17:13 +0000

    add Header component with user info hardcoded as grumpy 19

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Northcoder News</h1>
+      <div className="user-info">
+        <h2>grumpy19</h2>
+        <img
+          src="https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest?cb=20170707233013"
+          alt="user's image"
+          height="50px"
+          width="50px"
+        />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
