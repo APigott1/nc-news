a6f31c963c0103cee3f02c16feb0be2d613bf363
APigott1 <pigster99@gmail.com>
2025-11-11 13:06:29 +0000

    add default to home if path isn't routed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import ArticleList from "./components/ArticleList";
 import Header from "./components/Header";
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<ArticleList />} />
         <Route path="/articles/:article_id" element={<Article />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </>
   );
